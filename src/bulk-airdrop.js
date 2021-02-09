b5f48b6c827b69a9b4b39e0d225b2d96874ee7cd
think-in-universe <mstr.raphael@gmail.com>
2021-02-09 12:21:59 +0800

    update wallet url for testnet

--- src/bulk-airdrop.js
+++ src/bulk-airdrop.js
@@ -2,7 +2,10 @@ const nearAPI = require("near-api-js");
 const getConfig = require("./config");
 
 const BOATLOAD_OF_GAS = '100000000000000'
-const getWalletLink = (keyPair) => `https://redpacket.near.org/${keyPair.secretKey}`
+const getWalletLink = (env, keyPair) => {
+  const host = env === 'testnet' ? 'http://wallet.testnet.near.org/create/testnet' : 'https://redpacket.near.org';
+  return `${host}/${keyPair.secretKey}`;
+}
 
 function getKeyStore() {
   // Directory where Near credentials are going to be stored
@@ -29,11 +32,12 @@ async function getAccount(accountId) {
 
 async function createLinkdrops(number, amount) {
   const account = await getAccount(process.env.NEAR_ACCOUNT);
-  const rootAccount = process.env.NEAR_ENV === "mainnet" ? "near" : "testnet";
+  const env = process.env.NEAR_ENV;
+  const rootAccount = env === "mainnet" ? "near" : "testnet";
   const parsedAmount = nearAPI.utils.format.parseNearAmount(amount.toString())
   for (let i = 0; i < number; i++) {
     const keyPair = nearAPI.KeyPair.fromRandom('ed25519');
-    console.log(getWalletLink(keyPair));
+    console.log(getWalletLink(env, keyPair));
     await account.functionCall(rootAccount, 'send', { public_key: keyPair.publicKey.toString() }, BOATLOAD_OF_GAS, parsedAmount);
   }
 }
