eca46f37d0dab1ddcef7021ec03b75415e14e10b
think-in-universe <mstr.raphael@gmail.com>
2021-02-23 20:37:21 +0800

    update transfer script

--- src/transfer.js
+++ src/transfer.js
@@ -2,9 +2,9 @@ const nearAPI = require("near-api-js");
 const { getAccount } = require("./account");
 
 async function transfer(receiver, amount) {
-  const account = await getAccount(process.env.NEAR_ACCOUNT);
+  const sender = await getAccount(process.env.NEAR_ACCOUNT);
   const parsedAmount = nearAPI.utils.format.parseNearAmount(amount.toString())
-  await account.sendMoney(receiver, parsedAmount);
+  await sender.sendMoney(receiver, parsedAmount);
   console.log(`sent ${amount} Ⓝ  to ${receiver}`);
 }
 
